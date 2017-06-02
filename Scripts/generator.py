aa2d6e957a6a6c1fef2da54228f7558a23541245
XmuryNik <xmury.ya@yandex.ru>
2017-06-02 22:44:13 +0500

    update

--- Scripts/generator.py
+++ Scripts/generator.py
@@ -1,16 +1,13 @@
-def generator(space, style):
-	i = 0; html_1 = """
-		
+def generator(space):
+	i = 0; html = """
+		<style>
    			.brd {
    			border: 4px double black; /* Параметры границы */
     		padding: 10px; /* Поля вокруг текста */
    			}
   		</style>
-	"""
-	html = style[0:len(style)-8] + html_1
-	
-	print(html)
-	#print(style)
+	"""	
+	#print(html)
 	while i < len(space):
 		i+=1
 		line = '<div class = "brd">' + space[i][0] + '</div>\n'   
--- main.py
+++ main.py
@@ -9,20 +9,36 @@ from Scripts import parser as par
 from Scripts import preParser as pre
 from Scripts import generator as gen
 from Scripts import style 
-#import os
-#os.system("libreoffice --convert-to html Дозиметр\ Курск.docx")
-name_file = "Новыйдокумент.html"
+import os
+
+''' Алгоритм
+		* Перейти в директорию с документом
+		* Обнаружить документ .docx
+		* Переименовать по форме
+		* Конвертировать в html
+		* Переместить полученный документ в папку html
+
+	http://www.ilnurgi1.ru/docs/python/modules/os.html
+'''
 
 
+file = "test_1.html"
+
+out_dir_file = "./Documents/html/"
+
+link = "libreoffice --convert-to html "
+
+os.chdir("./Documents/docx")
+os.system(link + file)
 
 #print("text = ",style.styleParser(pre.preParser(name_file)))
 #(par.parser(pre.preParser(name_file)))
-space = par.parser(pre.preParser(name_file))
+space = par.parser(pre.preParser(out_dir_file + file))
 #space = parser(name_file)
-style = style.styleParser(pre.preParser(name_file))
+style = style.styleParser(pre.preParser(out_dir_file + file))
 '''
 for w in range(1, len(space)+1):
 	if space[w][1] == "<p":
 		print(w, "> ", space[w][0])
 '''
-gen.generator(space, style)
+gen.generator(space)
