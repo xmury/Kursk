bc04f328d428a93b421072fc27ca64fb050be7fb
Unknown <Xmury.ya@yandex.ru>
2017-06-03 08:30:31 +0200

    create prework()

--- main.py
+++ main.py
@@ -12,30 +12,38 @@ from Scripts import style
 import os
 
 ''' Алгоритм
-		* Перейти в директорию с документом
+		- Перейти в директорию с документом
 		* Обнаружить документ .docx
 		* Переименовать по форме
 		* Конвертировать в html
 		* Переместить полученный документ в папку html
 
-	http://www.ilnurgi1.ru/docs/python/modules/os.html
+	
 '''
+def prework():
+	i = 0
+	link = "libreoffice --convert-to html "
+	os.chdir("./Documents/docx") 	#* Перейти в директорию с документом
+	
+	for w in  os.listdir(): 		#* Обнаружить документ .docx
+		q = w[-5: len(w)]
+
+		if q == ".docx" or ".doc" in q: 	
+			i+=1; 	file = "doc_" + str(i) + ".html"
+			
+			os.rename(w, "doc_"+ str(i))	#	* Переименовать по форме
+			os.system(link + "doc_"+ str(i))	# 	* Конвертировать в html
+			os.system("mv " + file + " ../html")	#	* Переместить полученный документ в папку html
+	os. chdir("../html")	
+	return file
 
 
-file = "test_1.html"
-
-out_dir_file = "./Documents/html/"
-
-link = "libreoffice --convert-to html "
-
-os.chdir("./Documents/docx")
-os.system(link + file)
-
+file = prework()
 #print("text = ",style.styleParser(pre.preParser(name_file)))
 #(par.parser(pre.preParser(name_file)))
-space = par.parser(pre.preParser(out_dir_file + file))
+space = par.parser(pre.preParser(file))
 #space = parser(name_file)
-style = style.styleParser(pre.preParser(out_dir_file + file))
+style = style.styleParser(pre.preParser(file))
 '''
 for w in range(1, len(space)+1):
 	if space[w][1] == "<p":
