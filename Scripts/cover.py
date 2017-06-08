08af98c0bc7efeec522dca7a9acdd691ae061981
XmuryNik <xmury.ya@yandex.ru>
2017-06-08 22:12:48 +0500

    search gost-setting complite

--- Scripts/cover.py
+++ Scripts/cover.py
@@ -4,6 +4,7 @@ def cover_data(space, style):
 	g = 0;
 	f = open("data.html", "a")
 	f.write(style)
+	f.write("\n")
 	while g < len(space):
 		g = int(g) + 1;
 		text = str(g) + " !!! " + str(space[g][0]) + "\n"
